973d921793f7920b911e5a65381e9f0c67e059a1
MGokcayK <mgokcaykdev@gmail.com>
2025-02-09 13:58:24 +0000

    Adding target options for build command on overseer
    Changes add new output which default is `all` for cmake's `--target` option. This way, `cpp_build_clean` template redundant. Instead,  `clean` target can be used easily.

--- lua/overseer/template/user/cpp_build_release.lua
+++ lua/overseer/template/user/cpp_build_release.lua
@@ -3,10 +3,20 @@ return {
     builder = function()
         local root_dir = vim.fs.root(0, 'build')
         local build_dir = vim.fs.joinpath(root_dir, 'build')
+        local target = "all"
+        vim.ui.input(
+            {
+                prompt = "Which target you want to build? (Empty is default): ",
+            }, function(selected)
+                if selected and selected ~= "" then
+                    target = selected
+                end
+            end
+        )
         return {
             cmd = { "cmake" },
-            args = {"--build", build_dir , "--config Release"},
-            components = { { "on_output_quickfix", open = true }, "default" },
+            args = {"--build", build_dir, "--config Release", "--target", target},
+            components = { { "open_output", focus = false }, "default" },
         }
     end,
 }

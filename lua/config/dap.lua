8e1865750f7d701029a83c90276691ebb04d7414
MGokcayK <mgokcaykdev@gmail.com>
2025-02-19 22:23:25 +0000

    Adjust DAP default config with slightly different layout

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -15,7 +15,77 @@ require("dap-python").setup(vim.g.python3_host_prog)
 
 local dap = require('dap')
 local dapui = require("dapui")
-dapui.setup()
+
+config = {
+    icons = { expanded = "", collapsed = "", current_frame = "" },
+    mappings = {
+      -- Use a table to apply multiple mappings
+      expand = { "<CR>", "<2-LeftMouse>" },
+      open = "o",
+      remove = "d",
+      edit = "e",
+      repl = "r",
+      toggle = "t",
+    },
+    element_mappings = {},
+    expand_lines = vim.fn.has("nvim-0.7") == 1,
+    force_buffers = true,
+    layouts = {
+      {
+        -- You can change the order of elements in the sidebar
+        elements = {
+          -- Provide IDs as strings or tables with "id" and "size" keys
+          {
+            id = "scopes",
+            size = 0.50, -- Can be float or integer > 1
+          },
+          { id = "watches", size = 0.30 },
+          { id = "stacks", size = 0.10 },
+          { id = "breakpoints", size = 0.10 },
+        },
+        size = 40,
+        position = "left", -- Can be "left" or "right"
+    },
+    {
+        elements = {
+            { id = "console", size = 0.70 },
+            { id = "repl", size = 0.30 },
+        },
+        size = 20,
+        position = "bottom", -- Can be "bottom" or "top"
+      },
+    },
+    floating = {
+      max_height = nil,
+      max_width = nil,
+      border = "single",
+      mappings = {
+        ["close"] = { "q", "<Esc>" },
+      },
+    },
+    controls = {
+      enabled = vim.fn.exists("+winbar") == 1,
+      element = "repl",
+      icons = {
+        pause = "",
+        play = "",
+        step_into = "",
+        step_over = "",
+        step_out = "",
+        step_back = "",
+        run_last = "",
+        terminate = "",
+        disconnect = "",
+      },
+    },
+    render = {
+      max_type_length = nil, -- Can be integer or nil.
+      max_value_lines = 100, -- Can be integer or nil.
+      indent = 1,
+    },
+}
+
+dapui.setup(config)
 
 dap.configurations.cpp = {
   {
